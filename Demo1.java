643ef9c6f23a5d27f15c29990973133f4d8414de
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2019-10-31 15:32:07 +0530

    Complementary file for 4.4.java

---
+++ Demo1.java
@@ -0,0 +1,20 @@
+ // Sagnik Chattopadhyaya --- 16500117034
+
+package demo_pack;
+import java.util.*;
+
+public class Demo1{
+	//public static void main(String sagnik[]){
+	public void show(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers : ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		sum(a,b);
+	}
+	public void sum(int a,int b){
+		System.out.println("The sum is : "+a+b);
+	}
+
+	
+}
