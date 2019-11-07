384de41e5e38271663335fceece04560450b98b6
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2019-11-07 16:24:49 +0530

    Add files via upload

---
+++ 6.5.java
@@ -0,0 +1,56 @@
+ // Sagnik Chattopadhyaya --- 16500117034
+ //program to implement synchronisation
+
+
+class Caller implements Runnable{
+	String msg;
+	Thread t;
+	Callme  target;
+	Caller (String m,Callme sag){
+		//System.out.println("init");		
+		target  = sag;
+		msg = m;
+		t = new Thread(this);
+		t.start();
+	}
+	public void run(){
+	//System.out.println("In Caller");		
+	target.call(msg);
+	}
+}
+
+class Callme{
+	synchronized void call(String msg){
+		System.out.print("[ "+msg);
+		try{
+			Thread.sleep(200);
+		}
+		catch(InterruptedException e){
+			System.out.print("Interrupted");
+		}
+		System.out.println(" ]");
+	}
+}
+
+class CDemo{
+	public static void main(String Sagnik[]){
+		Callme ob = new Callme();
+		Caller t1 = new Caller("Hello",ob);
+		//System.out.println("1");
+		
+		Caller t2 = new Caller("CSE", ob);//Thread.sleep(200);
+		t2.t.setPriority(10);
+		//System.out.println("isalive?:"+t2.t.isAlive());
+		Caller t3 = new Caller("Students",ob);
+		//System.out.println("isalive?:"+t2.t.isAlive());
+		try{
+			t1.t.join();
+			//System.out.println("isalive?:"+t2.t.isAlive());
+			t2.t.join();
+			t3.t.join();
+		}
+		catch(InterruptedException e){
+			System.out.println("Interrupted Exception Found");
+		}
+	}
+}
